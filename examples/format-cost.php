4f286f479d54b86a95f1343a9e3353d73676303a
Ronald Borla <ronald.borla@yahoo.com>
2015-06-06 18:59:24 +0800

    Finalized initial stable release

--- examples/format-cost.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Borla\Chikka\Models\Cost;
-
-// Require autoload
-require('../vendor/autoload.php');
-
-// Adjust cost
-var_dump((new Cost(2, '09087800765'))->toArray());
--- examples/format-mobile-number.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Borla\Chikka\Models\Mobile;
-
-// Require autoload
-require('../vendor/autoload.php');
-
-// Print mobile
-echo (new Mobile('9087800765'));
